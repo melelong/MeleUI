a6e072e5b2edd637b0c82b41734df0b00119805f
mele <1137042726@qq.com>
2023-06-01 15:01:25 +0800

    完善组件文档

--- README.md
+++ README.md
@@ -1,25 +1,59 @@
-# MeleUI(学习编写组件)
-
-# MeleUI(组件库)
+# MeleUI
 
 ### 快速开始
 
+<p align="center">
+  <a target="_blank" href="https://melelong.github.io/MeleUI/" >
+    <img width="100" src="https://blog.melelong.com/static/upload/2023-4-30-14-59-24-427-472cf.webp" alt="logo">
+  </a>
+</p>
+
+<h2 align="center">
+  <p>MeleUI</p>
+  <p>仿elementUI风格的组件库</p>
+</h2>
+
 #### 1.安装组件库
+
+---
 
 ```bash
 npm i mele-ui
 ```
 
+> 请确保你的 Node.js 版本 >= 8.9。
+
 #### 2.引入组件库
 
+---
+
 ```javascript
-//  全部引入
-import 'mele-ui/dist/theme-chalk/index.css';
+//  完整引入
+import Vue from 'vue';
 import Ml from 'mele-ui';
+import 'mele-ui/dist/theme-chalk/index.css';
+import App from './App.vue';
+
 Vue.use(Ml);
 
+new Vue({
+  el: '#app',
+  render: (h) => h(App),
+});
+
 //  按需引用
-import 'mele-ui/dist/theme-chalk/card.css';
+import Vue from 'vue';
+// 引入卡片组件
 import { MlCard } from 'mele-ui';
+// 引入卡片组件样式
+import 'mele-ui/dist/theme-chalk/card.css';
+import App from './App.vue';
+
+// 注册卡片组件
 Vue.use(MlCard);
+
+new Vue({
+  el: '#app',
+  render: (h) => h(App),
+});
 ```
--- examples/components/ml/index.js
+++ examples/components/ml/index.js
@@ -1,26 +1,26 @@
 //  Layout 布局
-import MlCol from "@ml/col";
-import MlRow from "@ml/row";
+import MlCol from "./col";
+import MlRow from "./row";
 //  Container 布局容器
-import MlContainer from "@ml/container";
-import MlHeader from "@ml/header";
-import MlAside from "@ml/aside";
-import MlMain from "@ml/main";
-import MlFooter from "@ml/footer";
+import MlContainer from "./container";
+import MlHeader from "./header";
+import MlAside from "./aside";
+import MlMain from "./main";
+import MlFooter from "./footer";
 //  Icon 图标
-import MlIcon from "@ml/icon";
+import MlIcon from "./icon";
 //  Button 按钮
-import MlButton from "@ml/button";
-import MlButtonGroup from "@ml/button-group";
+import MlButton from "./button";
+import MlButtonGroup from "./button-group";
 //  Link 文字链接
-import MlLink from "@ml/link";
+import MlLink from "./link";
 //  Radio 单选框
-import MlRadio from '@ml/radio';
-import MlRadioGroup from '@ml/radio-group';
+import MlRadio from './radio';
+import MlRadioGroup from './radio-group';
 //  Dialog 对话框
-import MlDialog from "@ml/dialog";
+import MlDialog from "./dialog";
 //  Card 卡片
-import MlCard from "@ml/card";
+import MlCard from "./card";
 
 const components = [
   MlCol,
