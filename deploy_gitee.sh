b8e59dc6b7ff90aa446f385d2aea21480517e2ef
mele <1137042726@qq.com>
2023-06-02 02:51:45 +0800

    正式发布

--- deploy_gitee.sh
+++ deploy_gitee.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+# .sh脚本，win安装git，双击使用
 
 set -e
 npm run docs:build
@@ -7,5 +8,5 @@ git init
 git branch -M main
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/melelong/MeleUI.git main:docs-pages
+git push -f https://gitee.com/mele-long/mele-ui.git main:docs-pages
 cd -
